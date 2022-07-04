96e9df48042f50b9b94de4487f6adea3c1b8e7a1
Dragon25117 <rabvasya1994@yandex.ru>
2022-07-04 05:57:29 +0300

    3 урок без дз

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,20 +1,42 @@
+@extends('layouts.main')
+@section('header')
+<div class="row py-lg-5">
+  <div class="col-lg-6 col-md-8 mx-auto">
+    <h1 class="fw-light">Список новостей</h1>
 
+  </div>
+
+@endsection
+@section('content')
+@include('inc.data',['name'=>'Gb'])
 @forelse($newsList as $news)
-    <div class="news">
-        <h3><a href="{{ route('news.show', ['id' => $news['id']]) }}">
-                {{ $news['title'] }}
-            </a>
-        </h3>
+
+<div class="col">
+    <div class="card shadow-sm">
         <img src="{{ $news['image'] }}">
-        <br>
-        {{--Комментарий--}}
-        <p>Статус: <em>{{ $news['status'] }}</em></p>
-        <p>Автор: <em>{{ $news['author'] }}</em></p>
-        <p>{!! $news['description'] !!}</p>
-        <p>Категория {{ $news['categories'] }}</p>
+
+      <div class="card-body">
+        <strong>  <a href="{{ route('news.show', ['id' => $news['id']]) }}">
+            {{ $news['title'] }}
+        </a></strong>
+
+        <p class="card-text">Категория {{ $news['categories'] }}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+
+            <a href="{{ route('news.show', ['id' => $news['id']]) }}" class="btn btn-sm btn-outline-secondary">Подробнее</a>
+          </div>
+          <small class="text-muted">Автор: <em>{{ $news['author'] }}</em></small>
+          <small class="text-muted">Статус: <em>{{ $news['status'] }}</em></small>
+        </div>
+      </div>
     </div>
-    <hr>
+  </div> 
+
+
 @empty
 <h2>Новостей нет</h2>
 
 @endforelse
+@endsection
+
